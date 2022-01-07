8e468e53ec4438fc4b6a6086c0d4cb9843321c73
Rich Rosales <rich.rosales@boardriders.com>
2022-01-06 22:04:00 -0800

    Testing scss build

--- build.js
+++ build.js
@@ -1,76 +1,36 @@
-const StyleDictionaryPackage = require('style-dictionary');
-const {createArray} = require('./fns');
+const fs = require('fs-extra');
+const componentSCSS = require('./componentSCSS');
 
-// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
-
-StyleDictionaryPackage.registerFormat({
-  name: 'css/variables',
-  formatter: function (dictionary, config) {
-    return `${this.selector} {\n${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}\n}`
-  }
-});
-
-StyleDictionaryPackage.registerTransform({
-    name: 'sizes/px',
-    type: 'value',
-    matcher: function(prop) {
-        // You can be more specific here if you only want 'em' units for font sizes
-        return ["fontSizes", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
-    },
-    transformer: function(prop) {
-        // You can also modify the value here if you want to convert pixels to ems
-        return parseFloat(prop.original.value) + 'px';
-    }
-    });
-
-function getStyleDictionaryConfig(theme) {
-  return {
-    "source": [
-      `tokens/${theme}.json`,
-    ],
-    "format": {
-      createArray
-    },
-    "platforms": {
-      "web": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
-        "buildPath": `output/`,
-        "files": [{
-          "destination": `${theme}.json`,
-          "format": "createArray"
-        }, {
-          "destination": `${theme}.css`,
-          "format": "css/variables",
-          "selector": `.${theme}-theme`
-        }]
+module.exports = {
+  action: {
+    componentSCSS: {
+      do: function(dictionary, config) {
+        fs.ensureDirSync(config.buildPath);
+        var components = Object.keys(dictionary.properties.component);
+        components.forEach(function(component) {
+          let path = `${config.buildPath}${component}.scss`;
+          fs.writeFileSync(path, componentSCSS({
+            [component]: dictionary.properties.component[component]
+          }));
+          console.log(`✔ ${path}`);
+        })
       },
-      "scss": {
-        "transformGroup": "scss",
-        "files": [{
-          "destination": `${theme}-map.css`,
-          "format": "scss/map-deep",
-          "mapName": "my-tokens"
-        }]
+      undo: function(dictionary, config) {
+        var components = Object.keys(dictionary.properties.component);
+        components.forEach(function(component) {
+          fs.unlinkSync(`${config.buildPath}${component}.scss`, function(err) {
+            if (err) throw err;
+          });
+        })
       }
     }
-  };
+  },
+  source: [`tokens/**/*.json`],
+  platforms: {
+    scss: {
+      transformGroup: `scss`,
+      buildPath: `output/scss/`,
+      actions: [`componentSCSS`]
+    }
+  }
 }
-
-console.log('Build started...');
-
-// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
-
-['global', 'default'].map(function (theme) {
-
-    console.log('\n==============================================');
-    console.log(`\nProcessing: [${theme}]`);
-
-    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
-
-    StyleDictionary.buildPlatform('web');
-
-    console.log('\nEnd processing');
-})
-
-console.log('\n==============================================');
-console.log('\nBuild completed!');
